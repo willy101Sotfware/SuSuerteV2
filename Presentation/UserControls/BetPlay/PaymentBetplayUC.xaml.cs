4d2ff9e06be43aeace3e3e3dcdd32199ee826caf
William Ruiz <william.ruiz@ecity.com.co>
2025-06-24 11:48:50 -0500

    CancelPay

--- Presentation/UserControls/BetPlay/PaymentBetplayUC.xaml.cs
+++ Presentation/UserControls/BetPlay/PaymentBetplayUC.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using SuSuerteV2.Domain;
+using SuSuerteV2.Domain.ApiService;
 using SuSuerteV2.Domain.Enumerables;
+using SuSuerteV2.Domain.Peripherals;
 using SuSuerteV2.Domain.UIServices;
+using SuSuerteV2.Domain.Variables;
 using SuSuerteV2.Modals;
 using SuSuerteV2.UserControls;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -16,20 +20,24 @@ namespace SuSuerteV2.Presentation.UserControls.BetPlay
     /// </summary>
     public partial class PaymentBetplayUC : AppUserControl
     {
-      
+        private ArduinoController _peripherals;
         private const int ANIMATION_DELAY = 150;
         private const double SCALE_PRESSED = 0.95;
         private const double SCALE_NORMAL = 1.0;
-        private const decimal MONTO_MINIMO = 2000;
-        private const decimal MONTO_MAXIMO = 500000;
-        private const decimal INCREMENTO_VALIDO = 100;
+        private StateTransaction _tranStateTemp = StateTransaction.Iniciada;
 
 
         private ETypeTramites _ETypeTramites;
-        private Transaction _transaction;
+        private readonly Transaction _ts;
         private ModalWindow? _currentLoadModal = null;
         private TimerGeneric _timer;
         private DispatcherTimer _animationTimer;
+        private PaymentViewModel _paymentViewModel;
+
+        private bool _isPayCanceled = false;
+
+
+
         public PaymentBetplayUC()
         {
             InitializeComponent();
@@ -112,7 +120,7 @@ namespace SuSuerteV2.Presentation.UserControls.BetPlay
             {
          
                 case "btnCancelar":
-                    ProcessCancelar();
+                    ProcessCancelarAsync();
                     break;
 
             
@@ -140,19 +148,158 @@ namespace SuSuerteV2.Presentation.UserControls.BetPlay
         /// <summary>
         /// Procesa la acción de cancelar
         /// </summary>
-        private void ProcessCancelar()
+        /// 
+
+     
+        
+        private async Task ProcessCancelarAsync()
+        {
+            _ = Dispatcher.BeginInvoke(() => BtnCancel.Visibility = Visibility.Collapsed);
+
+            if (!_nav.ShowModal(Messages.CANCEL_TRANSACTION, new ConfirmationModal()))
+            {
+                _ = Dispatcher.BeginInvoke(() => BtnCancel.Visibility = Visibility.Visible);
+                return;
+            }
+            EventLogger.SaveLog(EventType.Info, "Pago cancelado por el usuario.");
+            await CancelPay();
+        }
+
+        private async Task CancelPay()
         {
             try
             {
-                EventLogger.SaveLog(EventType.Info, "RechargeUC", "Ingresando al evento ProcessCancelar", "OK");
-                SetCallBacksNull();
-                _timer?.Stop();
-                Navigator.Instance.NavigateTo(new MenuUC());
+                if (_paymentViewModel.IsPayCompleted) return;
+#if NO_PERIPHERALS
+#else
+                await _peripherals.StopAceptance();
+#endif
+                _isPayCanceled = true;
+                _tranStateTemp = StateTransaction.Cancelada;
+
+                if (_paymentViewModel.EnteredAmount > 0)
+                {
+                    _paymentViewModel.ReturnAmount = _paymentViewModel.EnteredAmount;
+                    _currentLoadModal = _nav.ShowLoadModal("Transacción cancelada. Devolución en curso...");
+                    ReturnMoney(_paymentViewModel.EnteredAmount);
+                }
+                else
+                {
+                    _currentLoadModal = _nav.ShowLoadModal("Transacción cancelada");
+                    _ts.DevueltaCorrecta = true;
+                    await SavePay();
+                }
+
             }
             catch (Exception ex)
             {
-                LogError("ProcessCancelar", ex);
+                EventLogger.SaveLog(EventType.Error, $"Ocurrió un error en tiempo de ejecución: {ex.Message}", ex);
             }
+        }
+
+
+        private void ReturnMoney(decimal returnValue)
+        {
+            _ts.DevueltaCorrecta = false;
+#if NO_PERIPHERALS
+            OnCashDispensed(returnValue, new Dictionary<int, int>());
+#else
+            _peripherals.StartDispenser(returnValue);
+#endif
+
+        }
+
+        private async void OnCashDispensed(decimal totalDispensed, Dictionary<int, int> details)
+        {
+
+            _paymentViewModel.DispensedAmount = totalDispensed;
+
+            _paymentViewModel.RemainingAmount = _paymentViewModel.ReturnAmount - _paymentViewModel.DispensedAmount;
+            string strValueToReturn = _paymentViewModel.RemainingAmount.ToString("C0");
+
+            SendDispenseDetails(details);
+
+            CloseLoadModal();
+
+            if (_paymentViewModel.DispensedAmount == _paymentViewModel.ReturnAmount)
+            {
+                _ts.DevueltaCorrecta = true;
+                await SavePay();
+            }
+            else
+            {
+                _currentLoadModal = _nav.ShowLoadModal("No se pudo entregar la totalidad del dinero hay un faltante de:" + $" {strValueToReturn} " + ".Por favor comunícate con un administrador.");
+                await Task.Delay(10000); // Timer para mostrar la modal y que se pueda leer
+                _ts.DevueltaCorrecta = false;
+                await SavePay();
+            }
+
+        }
+        private void SendDispenseDetails(Dictionary<int, int> details)
+        {
+
+            foreach (var denom in details.Keys)
+            {
+                var quantity = details[denom];
+                for (int i = 0; i < quantity; i++)
+                {
+                    SendTransactionDetail(TypeOperation.DP, Convert.ToDecimal(denom));
+                }
+            }
+        }
+        private void SendRejectDetails(Dictionary<int, int> details)
+        {
+
+            foreach (var denom in details.Keys)
+            {
+                var quantity = details[denom];
+                for (int i = 0; i < quantity; i++)
+                {
+                    SendTransactionDetail(TypeOperation.Reject, Convert.ToDecimal(denom));
+                }
+            }
+        }
+
+        private void SendTransactionDetail(TypeOperation op, decimal denom)
+        {
+            try
+            {
+                EventLogger.SaveLog(EventType.Info, $"Enviando detalle a la api: Op: {op}, Denom: {denom.ToString("C0")}");
+                Api.CreateTransactionDetail(op, (int)denom);
+
+            }
+            catch (Exception ex)
+            {
+                EventLogger.SaveLog(EventType.Error, $"Ocurrió un error en tiempo de ejecución: {ex.Message}", ex);
+            }
+        }
+
+        private void SetTransactionDescription()
+        {
+            switch (_tranStateTemp)
+            {
+                case StateTransaction.Aprobada:
+                    _ts.EstadoTransaccionVerb = "Exitoso";
+                    _ts.Descripcion += "Transacción finalizada correctamente. ";
+                    break;
+                case StateTransaction.Cancelada:
+                    _ts.EstadoTransaccionVerb = "Declinada";
+                    _ts.Descripcion += "Transacción Cancelada, No se realizó el pago.";
+                    break;
+                case StateTransaction.AprobadaSinNotificar:
+                    _ts.EstadoTransaccionVerb = "Exitoso";
+                    _ts.Descripcion += "Transacción aprobada, pero no se ha podido notificar el pago a la entidad correspondiente. ";
+                    break;
+                case StateTransaction.ErrorServicioTercero:
+                    _ts.EstadoTransaccionVerb = "Declinada";
+                    _ts.Descripcion += $"Transacción cancelada ocurrió un error en el servicio tercero, No se realizó el pago.";
+                    break;
+                default:
+                    break;
+            }
+
+            if (!_ts.DevueltaCorrecta)
+                _ts.Descripcion += $"Ocurrió un error durante la devolución del dinero. Cantidad faltante {_paymentViewModel.RemainingAmount.ToString("C0")}";
         }
 
         /// <summary>
@@ -215,6 +362,151 @@ namespace SuSuerteV2.Presentation.UserControls.BetPlay
                 _currentLoadModal.Close();
                 _currentLoadModal = null;
             }
+        }
+
+
+    public class PaymentViewModel : INotifyPropertyChanged
+    {
+       
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+
+        #region Attributes
+        private decimal _payAmount;
+
+        private decimal _enteredAmount;
+
+        private decimal _remainingAmount;
+
+        private decimal _returnAmount;
+
+        public decimal _dispensedAmount;
+
+        public bool _isReturnSuccess;
+
+        private List<Denomination> _denominations;
+
+
+        public List<Denomination> Denominations
+        {
+            get { return _denominations; }
+            set
+            {
+                _denominations = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Denominations)));
+            }
+        }
+
+        public decimal PayAmount
+        {
+            get { return _payAmount; }
+            set
+            {
+                if (_payAmount != value)
+                {
+                    _payAmount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PayAmount)));
+                }
+            }
+        }
+
+        
+        public decimal EnteredAmount
+        {
+            get { return _enteredAmount; }
+            set
+            {
+                if (_enteredAmount != value)
+                {
+                    _enteredAmount = value;
+                    RemainingAmount = (EnteredAmount < PayAmount) ? PayAmount - EnteredAmount : 0;
+                    ReturnAmount = (EnteredAmount > PayAmount) ? EnteredAmount - PayAmount : 0;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EnteredAmount)));
+                }
+            }
+        }
+
+        public decimal RemainingAmount
+        {
+            get { return _remainingAmount; }
+            set
+            {
+                if (_remainingAmount != value)
+                {
+                    _remainingAmount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(RemainingAmount)));
+                }
+            }
+        }
+
+        public decimal ReturnAmount
+        {
+            get { return _returnAmount; }
+            set
+            {
+                if (_returnAmount != value)
+                {
+                    _returnAmount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ReturnAmount)));
+                }
+            }
+        }
+
+        public decimal DispensedAmount
+        {
+            get { return _dispensedAmount; }
+            set
+            {
+                if (_dispensedAmount != value)
+                {
+                    _dispensedAmount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DispensedAmount)));
+                }
+            }
+        }
+
+        public bool IsPayCompleted
+        {
+            get { return _isReturnSuccess; }
+            set
+            {
+                if (_isReturnSuccess != value)
+                {
+                    _isReturnSuccess = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsPayCompleted)));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        public void RefreshAmountsList(int denomination, int quantity)
+        {
+            
+            var itemDenomination = Denominations.Where(d => d.DenominationValue == denomination).FirstOrDefault();
+            if (itemDenomination == null)
+            {
+                Denominations.Add(new Denomination
+                {
+                    DenominationValue = denomination,
+                    Quantity = quantity,
+                    TotalDenomAmount = denomination * quantity,
+                });
+                return;
+            }
+
+            itemDenomination.Quantity += quantity;
+            itemDenomination.TotalDenomAmount = denomination * itemDenomination.Quantity;
+        }
+
+        #endregion
+    }
+        public class Denomination
+        {
+            public decimal DenominationValue { get; set; }
+            public decimal Quantity { get; set; }
+            public decimal TotalDenomAmount { get; set; }
         }
 
     }
