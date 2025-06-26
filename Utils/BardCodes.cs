55ff3a71d94eb9f5bf900b07137e2452e705ef1e
William Ruiz <william.ruiz@ecity.com.co>
2025-06-26 15:51:38 -0500

    sin erroes barcode

--- Utils/BardCodes.cs
+++ Utils/BardCodes.cs
@@ -1,4 +1,5 @@
 ﻿using BarcodeLib.Barcode;
+using System.Drawing;
 using System.IO;
 
 namespace SuSuerteV2.Utils
@@ -8,36 +9,37 @@ namespace SuSuerteV2.Utils
         public static string CodeBar(string Factura)
         {
             byte[] base64SingleBytes;
-            string str2;
+            string str2 = string.Empty;
+
             try
             {
-                string str = "";
-
+                string str = string.Empty;
                 int num3 = 0;
                 int num4 = 0x3b9ac9ff;
+
                 for (int i = 0; i < 50; i++)
                 {
-                    Linear barcode = new()
+                    Linear barcode = new Linear
                     {
                         Type = BarcodeType.CODABAR,
-                        Data = string.Concat(Factura),
-                        // barcode.Data = string.Concat("(415)" + Code + "(8020)" + string.Format(Factura).PadLeft(10, '0') + "(3900)" + string.Format("{0:0}", ValorFactura).PadLeft(10, '0') + "(96)" + fecha);
+                        Data = Factura,
                         UOM = UnitOfMeasure.PIXEL,
                         BarWidth = 2,
                         BarHeight = 30,
-                        TextFont = new System.Drawing.Font("Arial", 8, System.Drawing.FontStyle.Bold)
+                        TextFont = new Font("Arial", 8, System.Drawing.FontStyle.Bold)
                     };
+
                     base64SingleBytes = barcode.drawBarcodeAsBytes();
-                    int length = base64SingleBytes.GetLength(0);
-                    if (length > num3) { num3 = length; }
-                    if (length < num4) { num4 = length; }
+                    int length = base64SingleBytes.Length;
+
+                    if (length > num3) num3 = length;
+                    if (length < num4) num4 = length;
+
                     if ((num3 != length) && (num4 < num3))
                     {
-                        byte[] inArray = base64SingleBytes;
-                        str = string.Format("data:image/png;base64," + Convert.ToBase64String(inArray), new object[0]);
-
-                        File.WriteAllBytes(@"C:\Users\Administrator\Desktop\WPFGANA\WPFGANA\Barcode\code.png", inArray);
-
+                        str = "data:image/png;base64," + Convert.ToBase64String(base64SingleBytes);
+                        Directory.CreateDirectory(@"C:\Users\Administrator\Desktop\WPFGANA\WPFGANA\Barcode\");
+                        File.WriteAllBytes(@"C:\Users\Administrator\Desktop\WPFGANA\WPFGANA\Barcode\code.png", base64SingleBytes);
                         break;
                     }
                 }
@@ -45,7 +47,7 @@ namespace SuSuerteV2.Utils
             }
             catch (Exception ex)
             {
-                throw ex;
+                throw;
             }
 
             return str2;
